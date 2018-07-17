902f7e87ee0db0b69b26b2b8d14b668b8295994b
Grant Chen <grant@the-allstars.com>
2018-07-17 14:57:40 +0800

    added NestedSetModel frameowrk.

---
+++ NestedSetModel/NestedSetModel.h
@@ -0,0 +1,19 @@
+//
+//  NestedSetModel.h
+//  NestedSetModel
+//
+//  Created by Chen Hsueh Cheng on 2018/7/17.
+//  Copyright © 2018年 Hsueh Cheng Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NestedSetModel.
+FOUNDATION_EXPORT double NestedSetModelVersionNumber;
+
+//! Project version string for NestedSetModel.
+FOUNDATION_EXPORT const unsigned char NestedSetModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NestedSetModel/PublicHeader.h>
+
+
